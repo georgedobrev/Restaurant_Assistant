2cc4929c8e1aef9d682dda67d84539a33870702c
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-05-28 10:55:07 +0300

    [RA-32] Navbar
    Create Navbar for Admin Dashboard

---
+++ Front-end/Restaurant Web/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Sidebar from './components/sidebar/SideBar'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <Sidebar/>
+    </div>
+    </>
+  )
+}
+
+export default App
