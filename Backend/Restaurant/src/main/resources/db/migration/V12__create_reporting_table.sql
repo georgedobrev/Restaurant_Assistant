4b5eda3ce83622341a98b0e92ae27f3fa933837b
PetkoLyuts <petko.lyutskanov@blankfactor.com>
2023-05-31 12:53:01 +0300

    Fix syntax

--- Backend/Restaurant/src/main/resources/db/migration/V12__create_reporting_table.sql
+++ Backend/Restaurant/src/main/resources/db/migration/V12__create_reporting_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE reporting
     restaurant_id INT,
     report_from INT,
     report_to INT,
-    FOREIGN KEY (table_id) REFERENCES table (id),
+    FOREIGN KEY (table_id) REFERENCES [table] (id),
     FOREIGN KEY (restaurant_id) REFERENCES restaurant (id),
     FOREIGN KEY (report_from) REFERENCES [user] (id),
     FOREIGN KEY (report_to) REFERENCES [user] (id)
