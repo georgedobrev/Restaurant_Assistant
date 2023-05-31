d47d9f3f763e9b2bb459981c3669f0a77735d8b4
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-05-31 11:47:12 +0300

    [RA-15] Notifications Page

--- Front-end/restaurant-app/App.js
+++ Front-end/restaurant-app/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { LoginScreen } from "./screens/Login Screen/LoginScreen";
-import Dashboard from "./screens/Notifications Screen/Dashboard";
+import { Dashboard } from "./screens/Notifications Screen/Dashboard";
 
 const Stack = createStackNavigator();
 
