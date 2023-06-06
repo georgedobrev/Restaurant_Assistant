9b68461e1b5be3198a422f7cd0539bca9909b312
Dame Stoilovski <dame.stoilovski@blankfactor.com>
2023-06-06 17:14:37 +0300

    Update QrCodeRepository.java

--- Backend/Restaurant/src/main/java/com/blankfactor/ra/repository/QrCodeRepository.java
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/repository/QrCodeRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QrCodeRepository extends JpaRepository<QrCode, Integer> {
-    List<QrCode> findByAppTableIn(List<AppTable> appTables);
 }
