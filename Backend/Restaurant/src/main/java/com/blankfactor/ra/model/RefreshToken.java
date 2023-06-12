9ff02d431d489655b198f1097ea54651e1d5487c
Dame Stoilovski <131446912+DameStoilovski99@users.noreply.github.com>
2023-06-12 18:58:51 +0300

    Initial commit

---
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/model/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.blankfactor.ra.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue
+    public Integer id;
+    @Column(unique = true)
+    public String token;
+    public boolean revoked;
+    public boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public AppUser appUser;
+}
---
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/security/jwt/LogoutService.java
@@ -0,0 +1,42 @@
+package com.blankfactor.ra.security.jwt;
+import com.blankfactor.ra.exceptions.custom.UserException;
+import com.blankfactor.ra.model.AppUser;
+import com.blankfactor.ra.repository.RefreshTokenRepository;
+import com.blankfactor.ra.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler{
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        String username = jwtService.extractUsername(jwt);
+
+        deleteUserToken(username);
+        SecurityContextHolder.clearContext();
+    }
+
+    @Transactional
+    private void deleteUserToken(String username) {
+        AppUser appUser = userRepository.findAppUserByEmail(username).orElseThrow(()  -> new UserException("User not found"));
+        refreshTokenRepository.deleteByAppUser(appUser);
+    }
+}
