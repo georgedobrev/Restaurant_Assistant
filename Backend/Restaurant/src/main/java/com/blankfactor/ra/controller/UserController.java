a13c5015e8ae5cec7e2241a295466a91a8d49beb
PetkoLyuts <petko.lyutskanov@blankfactor.com>
2023-06-13 10:05:14 +0300

    Skeleton

--- Backend/Restaurant/src/main/java/com/blankfactor/ra/controller/UserController.java
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.blankfactor.ra.controller;
 
 import com.blankfactor.ra.dto.UpdateUserDto;
-import com.blankfactor.ra.dto.UserDto;
 import com.blankfactor.ra.model.AppUser;
 import com.blankfactor.ra.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,16 +13,9 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping()
-    public ResponseEntity<AppUser> createUser(@RequestBody UserDto userDto) {
-        AppUser createdAppUser = userService.createUser(userDto);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdAppUser);
-    }
-
     @PostMapping("/addRole")
-    public ResponseEntity<AppUser> addRoleToUser(@RequestBody UserDto userDto) {
-        AppUser createdAppUser = userService.addRoleToUser(userDto);
+    public ResponseEntity<AppUser> addRoleToUser(@RequestBody UpdateUserDto updateUserDto) {
+        AppUser createdAppUser = userService.addRoleToUser(updateUserDto);
 
         return ResponseEntity.ok(createdAppUser);
     }
--- Backend/Restaurant/src/main/java/com/blankfactor/ra/dto/AuthenticationRequestDto.java
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/dto/AuthenticationRequestDto.java
@@ -5,13 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto {
-    private String name;
-    private String surname;
+public class AuthenticationRequestDto {
+
     private String email;
     private String password;
 }
