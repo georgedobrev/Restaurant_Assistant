f9878dfa993b308e5833dc43168166fd9c500b95
PetkoLyuts <petko.lyutskanov@blankfactor.com>
2023-06-15 11:43:03 +0300

    Add get user by email functionality

---
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/dto/UserEmailDto.java
@@ -0,0 +1,17 @@
+package com.blankfactor.ra.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserEmailDto {
+
+    @Email(message = "Email is not valid", regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+    @Size(max = 100, message = "Email cannot be more than 100 characters")
+    @NotNull(message = "Email cannot not be null")
+    private String email;
+}
