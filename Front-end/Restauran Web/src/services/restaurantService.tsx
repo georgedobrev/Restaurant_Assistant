706218c6a5540ffff01b0ce0653020d0cffe824f
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-06-16 14:22:36 +0300

    Endpoints

--- Front-end/Restauran Web/src/services/restaurantService.tsx
+++ Front-end/Restauran Web/src/services/restaurantService.tsx
@@ -1,5 +1,5 @@
 import { fetchWrapper } from "./fetchWrapper";
-import { baseUrl, restaurantsEndpoint } from "./config.json";
+import { baseUrl, restaurantsEndpoint, restaurants } from "./config.json";
 
 export interface Restaurant {
   id?: number;
@@ -10,18 +10,32 @@ export interface Restaurant {
 }
 
 export const getRestaurants = async (): Promise<Restaurant[]> => {
-  return fetchWrapper.get<Restaurant[]>(`${baseUrl}${restaurantsEndpoint}/getAll`);
+  return fetchWrapper.get<Restaurant[]>(
+    `${baseUrl}${restaurantsEndpoint}${restaurants}`
+  );
 };
 
 export const getRestaurantByID = async (id: number): Promise<Restaurant> => {
-  const response = await fetchWrapper.get<Restaurant[]>(`${baseUrl}${restaurantsEndpoint}/${id}`);
+  const response = await fetchWrapper.get<Restaurant[]>(
+    `${baseUrl}${restaurantsEndpoint}/${id}`
+  );
   return response[0];
 };
 
-export const editRestaurant = async (restaurantData: Restaurant): Promise<Restaurant> => {
-  return fetchWrapper.put<Restaurant>(`${restaurantsEndpoint}/${restaurantData.id}`, restaurantData);
+export const editRestaurant = async (
+  restaurantData: Restaurant
+): Promise<Restaurant> => {
+  return fetchWrapper.put<Restaurant>(
+    `${baseUrl}${restaurantsEndpoint}/${restaurantData.id}`,
+    restaurantData
+  );
 };
 
-export const createRestaurant = async (restaurantData: Restaurant): Promise<Restaurant> => {
-  return fetchWrapper.post<Restaurant>(`${baseUrl}${restaurantsEndpoint}`, restaurantData);
+export const createRestaurant = async (
+  restaurantData: Restaurant
+): Promise<Restaurant> => {
+  return fetchWrapper.post<Restaurant>(
+    `${baseUrl}${restaurantsEndpoint}`,
+    restaurantData
+  );
 };
