05f07e02b3d74aa6235a0185d60a3fd65d7088a9
PetkoLyuts <131446999+PetkoLyuts@users.noreply.github.com>
2023-06-19 13:44:44 +0300

    [RA-118] Create New JWT Token From Refresh Token (#59)
    * Implement endpoint
    
    * Address pr comments

--- Backend/Restaurant/src/main/java/com/blankfactor/ra/dto/RegisterRequest.java
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/dto/RegisterRequest.java
@@ -1,14 +1,10 @@
 package com.blankfactor.ra.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class RegisterRequest {
 
     private String name;
