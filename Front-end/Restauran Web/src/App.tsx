65dd13566bcd5b66f9a14da7981add81242c7267
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-06-26 12:06:42 +0300

    [RA-148] Login and Customer Screen

--- Front-end/Restauran Web/src/App.tsx
+++ Front-end/Restauran Web/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 import Sidebar from "./components/sidebar/SideBar";
 import Dashboard from "./screens/admin/Dashboard/Dashboard";
@@ -7,22 +8,48 @@ import Restaurant from "./screens/admin/Restaurant/Restaurant";
 import Tables from "./screens/admin/Tables/Tables";
 import Transactions from "./screens/admin/Transactions/Transactions";
 import Users from "./screens/admin/Users/Users";
+import Customer from "./screens/customer/Customer";
+import LoginScreen from "./screens/login/LoginScreen";
 
 function App() {
-  return (
-    <div>
-      <Sidebar />
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userType, setUserType] = useState("");
 
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/restaurant" element={<Restaurant />} />
-        <Route path="/tables" element={<Tables />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/transactions" element={<Transactions />} />
-      </Routes>
-    </div>
-  );
+  const handleLoggedIn = (loggedIn: boolean) => {
+    setLoggedIn(loggedIn);
+  };
+
+  const handleUserType = (userType: string) => {
+    setUserType(userType);
+  };
+
+  if (loggedIn) {
+    if (userType === "user") {
+      return (
+        <div>
+          <Customer appUserId={1} appTableId={1} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Sidebar />
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/restaurant" element={<Restaurant />} />
+            <Route path="/tables" element={<Tables />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/transactions" element={<Transactions />} />
+          </Routes>
+        </div>
+      );
+    }
+  } else {
+    return (
+      <LoginScreen setLoggedIn={handleLoggedIn} setUserType={handleUserType} />
+    );
+  }
 }
 
 export default App;
--- Front-end/Restauran Web/src/screens/constants.tsx
+++ Front-end/Restauran Web/src/screens/constants.tsx
@@ -8,9 +8,16 @@ export const usersSections = {
   addUser: "addUser",
   editUser: "editUser",
   deleteUser: "deleteUser",
+  addUserRole: "addRole",
 };
 
 export const status = {
   successStatus: "success",
   failureStatus: "failure",
 };
+
+export const storedUserId = localStorage.getItem("userId");
+export const storedJWT = localStorage.getItem("token");
+export const storedRefreshToken = localStorage.getItem("refreshToken");
+export const mobileBreakPoint = 768;
+export const loginScreenImage = "src/assets/restaurant-banner.jpeg";
