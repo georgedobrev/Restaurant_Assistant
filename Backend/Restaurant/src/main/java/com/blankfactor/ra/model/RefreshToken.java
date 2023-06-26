2b751d14dd8756a9e56a38df395a810fbcd22bd1
PetkoLyuts <petko.lyutskanov@blankfactor.com>
2023-06-26 17:10:00 +0300

    Refactor RefreshToken migration and entity

--- Backend/Restaurant/src/main/java/com/blankfactor/ra/model/RefreshToken.java
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/model/RefreshToken.java
@@ -5,17 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-@Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 @Entity
 @Table(name = "refresh_token")
-public class RefreshToken {
+public class RefreshToken extends Audit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Integer id;
+    public int id;
 
     @OneToOne
     @JoinColumn(name = "app_user_id", unique = true)
--- Backend/Restaurant/src/main/resources/db/migration/V12__create_refresh_token_table.sql
+++ Backend/Restaurant/src/main/resources/db/migration/V12__create_refresh_token_table.sql
@@ -1,6 +1,12 @@
 CREATE TABLE refresh_token (
     id INT PRIMARY KEY IDENTITY,
+    created_by INT NOT NULL,
+    updated_by INT,
     app_user_id INT UNIQUE,
     token NVARCHAR(255) UNIQUE NOT NULL,
-    FOREIGN KEY (app_user_id) REFERENCES app_user(id)
+    created_at DATETIMEOFFSET NOT NULL,
+    updated_at DATETIMEOFFSET,
+    FOREIGN KEY (app_user_id) REFERENCES app_user(id),
+    FOREIGN KEY (created_by) REFERENCES app_user (id),
+    FOREIGN KEY(updated_by) REFERENCES app_user (id)
 )
