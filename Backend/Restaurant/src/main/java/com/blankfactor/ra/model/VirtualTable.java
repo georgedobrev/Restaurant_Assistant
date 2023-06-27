629eca146bb0fb9bc322ac425e5478471f5368be
PetkoLyuts <petko.lyutskanov@blankfactor.com>
2023-06-27 09:13:49 +0300

    Refactor VirtualTable migration and entity

--- Backend/Restaurant/src/main/java/com/blankfactor/ra/model/VirtualTable.java
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/model/VirtualTable.java
@@ -1,12 +1,18 @@
 package com.blankfactor.ra.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 @Table(name = "virtual_table")
-public class VirtualTable {
+public class VirtualTable extends Audit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id")
--- Backend/Restaurant/src/main/resources/db/migration/V7__create_virtual_table_table.sql
+++ Backend/Restaurant/src/main/resources/db/migration/V7__create_virtual_table_table.sql
@@ -1,5 +1,11 @@
 CREATE TABLE virtual_table
 (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    tables_ids NVARCHAR(50)
+    tables_ids NVARCHAR(50),
+    created_by INT NOT NULL,
+    updated_by INT,
+    created_at DATETIMEOFFSET NOT NULL,
+    updated_at DATETIMEOFFSET,
+    FOREIGN KEY (created_by) REFERENCES app_user (id),
+    FOREIGN KEY(updated_by) REFERENCES app_user (id)
 );
