f6751315d6611cf32b0ccedfe70930a7079235e1
PetkoLyuts <131446999+PetkoLyuts@users.noreply.github.com>
2023-07-03 13:18:41 +0300

    [RA-128] Separate websocket connections for waiters (#61)
    * Separate websocket connections for waiters
    
    * Fix
    
    * Refactor
    
    * Remove line
    
    * Remove unnecessary comment and add TODO

--- Backend/Restaurant/src/main/java/com/blankfactor/ra/controller/NotificationController.java
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/controller/NotificationController.java
@@ -60,11 +60,10 @@ public class NotificationController {
         return ResponseEntity.ok().build();
     }
 
+    // TODO: Add websocket direction for user
     @MessageMapping("/app/notify")
     @SendTo("/topic/notifications")
-    public Notification sendNotification(NotificationDto notificationDto) throws InterruptedException {
-        Thread.sleep(2000);
-
+    public Notification sendNotification(NotificationDto notificationDto) {
         return notificationService.createNotification(notificationDto);
     }
 }
