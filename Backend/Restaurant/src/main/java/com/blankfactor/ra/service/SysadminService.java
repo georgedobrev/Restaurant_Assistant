3d9ba0252cf09e782eb7822c7af8747e2638d52a
Dame Stoilovski <dame.stoilovski@blankfactor.com>
2023-07-10 09:23:43 +0300

    Update naming in MergedTablesImpl

--- Backend/Restaurant/src/main/java/com/blankfactor/ra/service/SysadminService.java
+++ Backend/Restaurant/src/main/java/com/blankfactor/ra/service/SysadminService.java
@@ -1,7 +1,6 @@
 package com.blankfactor.ra.service;
 
 import com.blankfactor.ra.dto.SysadminDto;
-import com.blankfactor.ra.exceptions.custom.SysadminException;
 import com.blankfactor.ra.model.Sysadmin;
 
 import java.util.List;
